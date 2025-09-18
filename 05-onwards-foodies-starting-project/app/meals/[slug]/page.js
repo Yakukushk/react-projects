2dc6eb382cd91e4ba06238a329a1850dd78c9c97
Danilka <daniilliubko@gmail.com>
2025-09-18 11:31:51 +0200

    update commit

---
+++ 05-onwards-foodies-starting-project/app/meals/[slug]/page.js
@@ -0,0 +1,7 @@
+export default function MealPage({params}) {
+   return (
+    <>
+    <h1>MealPage - {params.slug}</h1>
+    </>
+   )
+}
--- next/app/page.js
+++ next/app/page.js
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+      <div style={{ marginInline: '2px' }}>
+        <Link href={"/about"}>About Us</Link>
+        <Link style={{marginLeft: '10px'}} href={"/blog"}>Blog Page</Link>
+      </div>
     </main>
   );
 }
